04ec7ddff1e0ea668703fd3a12a115499722d097
Kaden <kadenw258@gmail.com>
2024-04-17 00:33:39 -0500

    UI progress
    Almost completed the UI for the settings button

--- DynamicAnimations/src/Interface.jsx
+++ DynamicAnimations/src/Interface.jsx
@@ -1,17 +1,29 @@
+import React, { useRef } from 'react';
+
 function Buttons() {
+  const pageContainerRef = useRef(null);
+
+  const handleButtonClick = () => {
+    if (pageContainerRef.current) {
+      pageContainerRef.current.style.setProperty('--start', '0%');
+      pageContainerRef.current.style.setProperty('--end', '100%');
+      console.log("Button Clicked")
+    }
+  };
+
   return (
     <>
-    <div id = "Settings">
-        <button>+</button>
-    </div>
-    <div className="PageContainer">
-    <div className="Buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-    </div>
-    </div>
+      <div id="Settings">
+        <button onClick={handleButtonClick}>+</button>
+      </div>
+      <div className="PageContainer" ref={pageContainerRef} style={{ '--start': '100%', '--end': '0%' }}>
+        <div className="Buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+        </div>
+      </div>
     </>
   );
 }
