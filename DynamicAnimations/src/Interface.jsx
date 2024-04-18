fc342f2eb805b172218041f33f2673b0d557d035
Kaden <kadenw258@gmail.com>
2024-04-18 00:35:09 -0500

    Basic UI Implementation
    Added a sliding panel with buttons for later changes to the animation

--- DynamicAnimations/src/Interface.jsx
+++ DynamicAnimations/src/Interface.jsx
@@ -1,16 +1,48 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 function Buttons() {
   const pageContainerRef = useRef(null);
+  const [Buttoncount, setButtoncount] = useState(0);
+  const [Timer, setTimer] = useState(0);
+  useEffect(() => {
+  const interval = setInterval(() => {
+      setTimer(prev => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   const handleButtonClick = () => {
-    if (pageContainerRef.current) {
+    if (pageContainerRef.current && Buttoncount === 0 && Timer > 2.5) {
+      // Remove the animation
+      pageContainerRef.current.style.animation = 'none';
+      // Force a reflow
+      pageContainerRef.current.offsetHeight; // reflow
+      // Set animation
       pageContainerRef.current.style.setProperty('--start', '0%');
       pageContainerRef.current.style.setProperty('--end', '100%');
-      console.log("Button Clicked")
+      pageContainerRef.current.style.animation = 'Slide 1s linear forwards';
+      setButtoncount(1);
+      setTimer(0)
+      console.log(Buttoncount);
+    }
+    if (pageContainerRef.current && Buttoncount === 1 && Timer > 2.5) {
+      // Remove the animation
+      pageContainerRef.current.style.animation = 'none';
+      // Force a reflow
+      pageContainerRef.current.offsetHeight; // reflow
+      // Set animation
+      pageContainerRef.current.style.setProperty('--start', '100%');
+      pageContainerRef.current.style.setProperty('--end', '0%');
+      pageContainerRef.current.style.animation = 'Slide 1s linear forwards';
+      setButtoncount(0);
+      setTimer(0)
+      console.log(Buttoncount);
+
     }
   };
-
   return (
     <>
       <div id="Settings">
